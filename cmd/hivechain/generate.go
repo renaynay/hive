cfefe6d8d1e5bfaf3c0b5198ca1f32b79051cdaa
renaynay <41963722+renaynay@users.noreply.github.com>
2021-03-31 16:18:44 +0200

    debug

--- cmd/hivechain/generate.go
+++ cmd/hivechain/generate.go
@@ -90,8 +90,8 @@ func (cfg generatorConfig) addTxForKnownAccounts(i int, gen *core.BlockGen) {
 	txCount := 0
 
 	var gasLimit uint64
-	if gen.Number().Uint64() > 1 {
-		gasLimit = core.CalcGasLimit(gen.PrevBlock(int(gen.Number().Int64()-1)), 0, cfg.genesis.GasLimit)
+	if gen.Number().Uint64() > 0 {
+		gasLimit = core.CalcGasLimit(gen.PrevBlock(-1), 0, cfg.genesis.GasLimit)
 	}
 	fmt.Println("GAS LIMIT: ", gasLimit)
 	var sumGas uint64
